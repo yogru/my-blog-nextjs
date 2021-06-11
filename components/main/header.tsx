8ce9baf4ded51abe5a7c97e6128dd9157a1a6e46
kyb <youngbok@afsmed.com>
2021-06-11 15:34:06 +0900

    기본적인 틀 완성

---
+++ components/main/header.tsx
@@ -0,0 +1,18 @@
+import Box from '@material-ui/core/Box'
+
+interface Props{
+
+}
+
+
+function MainHeader(props:Props) {
+
+    return (
+        <Box display={'flex'} height="4rem" bgcolor="blue">
+            뭐지..?
+        </Box>
+    )
+}
+
+
+export default MainHeader
---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import MainHader from '../components/main/header'
+import Box from '@material-ui/core/Box'
+
+function IndexPage(){
+
+
+    return (
+        <Box flex flexDirection="column">
+            <MainHader/>
+        </Box>
+
+
+        )
+
+
+}
+
+
+export default IndexPage
