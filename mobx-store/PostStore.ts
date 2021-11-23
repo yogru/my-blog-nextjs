2d34e459a773f9c6f966b63b4c116bac4dad3d13
yogru <kybdev@gmail.com>
2021-11-23 22:28:52 +0900

    <:feat> 태그는 반드시 하나 이상 갖도록

--- mobx-store/PostStore.ts
+++ mobx-store/PostStore.ts
@@ -18,8 +18,10 @@ export class PostStoreImp  implements PostStore{
 
     public async submit(title:string, body:string){
         const userModel =  this.rootStore.getUserStore().getUser()
+
+        // 태그가 없을 땐 none으로 설정.
         const postModel = new Post({
-            title,body,tags:[], editor:userModel
+            title,body,tags:["none"], editor:userModel
         })
         // PostModel.createByView(title,body,[],userModel)
         return  PostRepository.createPost(postModel)
